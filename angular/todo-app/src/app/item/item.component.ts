d52e88f88291dd6b220075209ad7730a302f6d61
Amar Bešlija <amarbeslija13@gmail.com>
2020-12-05 19:23:53 +0100

    Third inital upload of all three apps

---
+++ angular/todo-app/src/app/item/item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import ToDoItem from '../model/ToDoItem';
+import { ItemsService } from '../services/items.service';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() item: ToDoItem;
+
+  constructor(public itemsService: ItemsService) { 
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
